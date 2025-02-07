93212d29c736330b5375af83913ef8ce0cbc9785
Dwaipayan Datta <dwaidatta@gmail.com>
2025-02-07 21:57:15 +0530

    folders and js day 1

---
+++ class/js-class/class1.js
@@ -0,0 +1,94 @@
+console.log("Hello World from file")
+// comment here
+/* multi line comment*/
+
+var x = 10;
+var y = 5;
+var sum = x + y;
+console.log(sum);
+
+const a = 20;
+console.log(a);
+
+let b = 55;
+console.log(b);
+
+var Student = "dwai";
+var sTudent = "hmm";
+
+console.log(Student);
+console.log(sTudent);
+
+var numbers = 12;
+var boolean = true;
+
+// arthimetic operation
+sum = 10 + 10;
+console.log(sum);
+console.log(11 * 10);
+console.log(60/10);
+console.log(8+5-3+8*7+9/3);
+
+var deci1 = 3.14;
+var deci2= 6.48;
+console.log(deci1 + deci2);
+
+var num = 33;
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num)
+
+x = 11;
+console.log(x%2);
+
+var p = 13;
+var q = 14;
+var r = 15;
+
+p += 3;
+console.log(p);
+q -= 3;
+console.log(q);
+r *= 3;
+console.log(r);
+
+var h = "dwai";
+console.log(h);
+
+var line1 = "Hello world \"this\" is a line";
+console.log(line1);
+
+var str = "<a href= \"https://meet.google.com/hru-hqvu-bvw\" target=\"_blank\">Link to GMeet</a>"
+console.log(str)
+
+line1 = "Hello world \"this\" is a line, three \\\\\\ newline in \nnewline";
+console.log(line1);
+
+var s1 = "1str-one";
+var s2 = "2str-two";
+console.log(s1 + s2);
+
+s1 += s2;
+console.log(s1);
+
+var len = s1.length;
+console.log(len);
+
+// bracket notation
+var letter = s1[10];
+console.log(letter);
+
+var word1 = s1[len - 5]
+console.log(word1)
+
+// logical and relational operators
+
+let x1 = 6;
+let y1 = "6";
+
+console.log(x1 == y1); // gives true
+console.log(x1 != y1); // gives false
+
+
